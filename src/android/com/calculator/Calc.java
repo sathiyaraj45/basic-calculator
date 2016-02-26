ff7dc1abcc5c147dfc5c71c708a38a76f9b84f64
sathiyarajr@techaffinity.com <sathiyarajr>
2016-02-26 14:24:23 +0530

    multiple method added

--- src/android/com/calculator/Calc.java
+++ src/android/com/calculator/Calc.java
@@ -15,7 +15,6 @@ public class Calc extends CordovaPlugin {
 
     public Calc(){}
 
-    
     @Override
     public boolean execute(String action, JSONArray args,final CallbackContext callbackContext) throws JSONException {
         JSONObject data = args.getJSONObject(0);
@@ -23,6 +22,18 @@ public class Calc extends CordovaPlugin {
             double a = Double.parseDouble((String) data.get("firstValue"));
             double b = Double.parseDouble((String) data.get("secondValue"));
             addition(a, b, callbackContext);
+        } else if (action.equals("sub")) {
+            double a = Double.parseDouble((String) data.get("firstValue"));
+            double b = Double.parseDouble((String) data.get("secondValue"));
+            subtraction(a, b, callbackContext);
+        } else if (action.equals("mul")) {
+            double a = Double.parseDouble((String) data.get("firstValue"));
+            double b = Double.parseDouble((String) data.get("secondValue"));
+            multiplication(a, b, callbackContext);
+        } else if (action.equals("div")) {
+            double a = Double.parseDouble((String) data.get("firstValue"));
+            double b = Double.parseDouble((String) data.get("secondValue"));
+            division(a, b, callbackContext);
         }
 
         return false;
@@ -38,5 +49,34 @@ public class Calc extends CordovaPlugin {
         return true;
     }
 
+    public boolean subtraction(double a, double b, CallbackContext callbackContext) throws JSONException {
+        JSONObject res = new JSONObject();
+        double c = a - b;
+        res.put("firstValue", a);
+        res.put("secondValue", b);
+        res.put("result", c);
+        callbackContext.success(res);
+        return true;
+    }
+
+    public boolean multiplication(double a, double b, CallbackContext callbackContext) throws JSONException {
+        JSONObject res = new JSONObject();
+        double c = a * b;
+        res.put("firstValue", a);
+        res.put("secondValue", b);
+        res.put("result", c);
+        callbackContext.success(res);
+        return true;
+    }
+
+    public boolean division(double a, double b, CallbackContext callbackContext) throws JSONException {
+        JSONObject res = new JSONObject();
+        double c = a / b;
+        res.put("firstValue", a);
+        res.put("secondValue", b);
+        res.put("result", c);
+        callbackContext.success(res);
+        return true;
+    }
 }
 
--- www/Calc.js
+++ www/Calc.js
@@ -11,6 +11,29 @@ success
 cordova.exec(successCallback, errorCallback, "Calc", "add", [options])
 }
 
+Calc.prototype.subtraction = function(successCallback, errorCallback, options){
+/*
+success
+*/
+cordova.exec(successCallback, errorCallback, "Calc", "sub", [options])
+}
+
+
+Calc.prototype.multiplication = function(successCallback, errorCallback, options){
+/*
+success
+*/
+cordova.exec(successCallback, errorCallback, "Calc", "mul", [options])
+}
+
+
+Calc.prototype.division = function(successCallback, errorCallback, options){
+/*
+success
+*/
+cordova.exec(successCallback, errorCallback, "Calc", "div", [options])
+}
+
 var calc = new Calc();
 module.exports = calc;
 
